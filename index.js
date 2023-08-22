63fbe7caef55b17de5343fb927578986ca4bd572
Bogdan <bogdan_mykhailov@icloud.com>
2023-08-22 13:25:01 +0300

    :construction: implement page routing and product template

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fs = require('fs')
 const http = require('http');
 const url = require('url');
 
-
 // sync way, blocking
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -44,50 +43,50 @@ const url = require('url');
 //   });
 // });
 const port = 8000
-
 const replaceTemplate = (template, product) => {
   let output = template.replace(/{%PRODUCT_NAME%}/g, product.productName);
   output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%NUTRIENTS_NAME%}/g, product.nutrients);
   output = output.replace(/{%PRICE%}/g, product.price);
   output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS_NAME%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
   output = output.replace(/{%ID%}/g, product.id);
   if (!product.organic) {
-    output = output.replace('{%NOT_ORGANIC%}', 'not-organic');
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
   }
 
   return output;
 }
+const templateOverview = fs.readFileSync('./templates/template-overview.html', 'utf-8');
+const templateProduct = fs.readFileSync('./templates/template-product.html', 'utf-8');
+const templateCard = fs.readFileSync('./templates/template-card.html', 'utf-8');
 
-const templateOverview = fs.readFileSync('./templates/template-overview.html', 'utf-8')
-const templateProduct = fs.readFileSync('./templates/template-product.html', 'utf-8')
-const templateCard = fs.readFileSync('./templates/template-card.html', 'utf-8')
 const data = fs.readFileSync('./dev-data/data.json', 'utf-8')
 const productData = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
-  if (pathName === '/' || pathName === '/overview') {
-
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {'Content-Type': 'text/html'})
 
     const cardsHtml = productData.map((el) => replaceTemplate(templateCard, el)).join('');
-    const overview = templateOverview.replace(/{%PRODUCT_CARD%}/g, cardsHtml);
-    res.end(overview)
-  } else if (pathName === '/product') {
-    res.end('This is the product')
-  } else if (pathName === '/api') {
-
-
+    const output = templateOverview.replace('{%PRODUCT_CARD%}', cardsHtml);
+    res.end(output)
+  } else if (pathname === '/product') {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    const product = productData[query.id];
+    const output = replaceTemplate(templateProduct, product);
+    res.end(output)
+  } else if (pathname === '/api') {
     res.writeHead(200, {'Content-Type': 'application/json'})
     res.end(data);
   } else {
     res.writeHead(404, {
       'Content-Type': 'text/html',
-      'my-own-header': 'hello-world'
+      'my-own-header': 'hello-world',
     });
     res.end("<h1>Page not found</h1>");
   }
